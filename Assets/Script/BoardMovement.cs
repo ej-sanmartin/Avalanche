dc6213198220f6c87b61779270350fe2e9fc8dff
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2020-02-05 05:06:13 -0500

    added waypoints for board movement

---
+++ Assets/Script/BoardMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BoardMovement : MonoBehaviour {
+  public Transform[] waypoints;
+
+  [SerializeField]
+  private float moveSpeed = 1f;
+
+  [HideInInspector]
+  public int waypointIndex = 0;
+
+  public bool moveAllowed = false;
+
+  private void Start(){
+    transform.position = waypoints[waypointIndex].transform.position;
+  }
+
+  private void Update(){
+    if(moveAllowed){
+      Move();
+    }
+  }
+
+  private void Move(){
+    if(waypointIndex <= waypoints.Length - 1){
+      // transform.positon = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+      // TODO: when player reaches end, clear off path and add one point.
+      if(transform.position == waypoints[waypointIndex].transform.position){
+        waypointIndex += 1;
+      }
+    }
+  }
+}
