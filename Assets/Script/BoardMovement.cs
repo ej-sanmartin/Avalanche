cbecb75a5c7815413aed9e86a31f1238418cbca1
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2020-02-07 04:03:33 -0500

    more setup and Ui updates

--- Assets/Script/BoardMovement.cs
+++ Assets/Script/BoardMovement.cs
@@ -23,7 +23,7 @@ public class BoardMovement : MonoBehaviour {
 
   private void Move(){
     if(waypointIndex <= waypoints.Length - 1){
-      // transform.positon = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+      transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
 
       // TODO: when player reaches end, clear off path and add one point.
       if(transform.position == waypoints[waypointIndex].transform.position){
